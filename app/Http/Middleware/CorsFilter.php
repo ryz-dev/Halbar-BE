dd613e5c9723837f10a5a294f49824e0cff4ad63
zulkifli_r <muh.zulkifli@docotel.com>
2019-03-21 12:15:04 +0800

    update cors middleware

--- app/Http/Middleware/CorsFilter.php
+++ app/Http/Middleware/CorsFilter.php
@@ -18,7 +18,7 @@ class CorsFilter
     public function handle($request, Closure $next)
     {
         $headers = [
-            'Access-Control-Allow-Origin'      => 'http://devweb.docoblast.com',
+            'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',

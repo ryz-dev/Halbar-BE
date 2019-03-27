c3de46ac01b169510b7c1d5b9d143cbf247350e5
zulkifli_r <muh.zulkifli@docotel.com>
2019-03-27 14:47:32 +0800

    update image on opd endpoint

--- app/Http/Controllers/API/V1/OpdController.php
+++ app/Http/Controllers/API/V1/OpdController.php
@@ -16,8 +16,11 @@ class OpdController extends Controller
 {
     public function read($slug = null){
         $opd = Opd::with('category')->where('slug', $slug )->where('deleted_at', null)->first();
+
+        $opd->image = asset('uploaded/media/'.$opd->image);
         
         $opd['team'] =  \DB::table('pages_about_team')->where('id',$opd->team_id)->first();
+        $opd['team']->image = asset('uploaded/media/'.$opd['team']->image);
         
         if ($opd) {
             $data['response'] =$opd;
